89fd442a5145db8dbd18d1b12964675a80dfa9f3
root <root@Dreplica.localdomain>
2019-11-18 13:00:51 +0100

    include flask sql config for database

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 
 import cs50
 from flask import FLask, render_template,redirect,sessions
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+# alchemy is the connection we are to use btw sqlite and python 
 from flask_sqlalchemy import SQLAlchemy
+# from import the function to encrypt and decrypt password 
+app = Flask(__name__)
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+# you need to import sql to give it to a database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'splite:////emedic.db'
