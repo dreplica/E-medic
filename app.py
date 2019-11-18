fcc5034b38bdc210a7f5b60114d6720c5fe7f7d0
root <root@Dreplica.localdomain>
2019-11-18 13:04:26 +0100

    created first table, and edited the name of the database from emedic to medic.db

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 # you need to import sql to give it to a database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'splite:////emedic.db'
+#commence app with database
+db = SQLAlchemy(app)
+
+def apology(issue,code):
+    html = "<div><p>"+code+"</p>"+"<p>"+issue+"</p></div>"
+    return render_template('apology.html',html)
+
+class info(db.model):
+    id = db.column()
+    user_id = db.column()
+    user_id = db.column()
+    user_id = db.column()
+    user_id = db.column()
+    def __repr__(self):
+        return '<users %r>' % info.user_id
+
